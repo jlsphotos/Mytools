231a87a99c3c52551ab7fbc9814ac5d1c0849570
jlsphotos <admin@jlsphotos.co.uk>
2017-09-06 10:24:24 +0100

    first push

--- IMDB scraper.py
+++ IMDB scraper.py
@@ -1,55 +1,58 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as ureq
+'''
+Scrapes IMDB Top movies, then copys to a file. This very rough around the edges but it works  
+'''
+for year in range(2015,2018):
+    years = 2017
 
-url = 'http://www.imdb.com/search/title?year=2017,2017&title_type=feature&sort=moviemeter,asc'
+    url = 'http://www.imdb.com/search/title?year=%s,%s&title_type=feature&sort=moviemeter,asc' % (year,year)
 
-# set connections
-uclient = ureq(url)
-sauce = uclient.read()
-uclient.close()
+    # set connections
+    uclient = ureq(url)
+    sauce = uclient.read()
+    uclient.close()
 
-# Set parcer
-soup = bs(sauce, 'html.parser')
+    # Set parcer
+    soup = bs(sauce, 'html.parser')
 
-# Set CSV
-fn = 'Top_Movies.csv'
-header = 'Title, Cert, Rating, Votes, Runtime, Genre\n'
-f = open(fn, "w")
+    # Set CSV
+    fn = 'Top_Movies %s.csv' % str(year)
+    header = 'Title, Cert, Rating, Votes, Runtime, Genre\n'
+    f = open(fn, "w")
+    f.write(header)
 
-f.write(header)
+    containers = soup.find_all('div',{'class':'lister-item-content'})
+    for contain in containers:
 
+        title = contain.find_all('a')[0].text
+        try:
+            cert = contain.find_all('span',{'class':'certificate'})[0].text
+        except Exception:
+            cert = 'NA'
 
-containers = soup.find_all('div',{'class':'lister-item-content'})
-for contain in containers:
+        try:
+            rating = contain.find_all('div',{'class':'ratings-imdb-rating'})[0].text
+        except Exception:
+            rating = 0.0
 
-    title = contain.find_all('a')[0].text
-    try:
-        cert = contain.find_all('span',{'class':'certificate'})[0].text
-    except Exception:
-        cert = 'NA'
+        rating = str(rating)
+        try:
+            votes = contain.find_all('span',{'name':'nv'})[0].text
+        except Exception:
+            votes = 0
 
-    try:
-        rating = contain.find_all('div',{'class':'ratings-imdb-rating'})[0].text
-    except Exception:
-        rating = 0.0
+        try:
+            runtime = contain.find_all('span', {'class':'runtime'})[0].text
+        except Exception:
+            runtime = 'Unknown'
 
-    rating = str(rating)
-    try:
-        votes = contain.find_all('span',{'name':'nv'})[0].text
-    except Exception:
-        votes = 0
+        try:
+            genre = contain.find_all('span', {'class': 'genre'})[0].text
+        except Exception:
+            genre = 'NA'
 
-    try:
-        runtime = contain.find_all('span', {'class':'runtime'})[0].text
-    except Exception:
-        runtime = 'Unknown'
+        f.write(title + ',' + cert + ',' + str(rating).strip() + ',' +
+                str(votes).replace(',', '') + ',' + runtime + ',' + genre.replace(',', ' |').strip() + ',\n')
 
-    try:
-        genre = contain.find_all('span', {'class': 'genre'})[0].text
-    except Exception:
-        genre = 'NA'
-
-    f.write(title + ',' + cert + ',' + str(rating).strip() + ',' +
-            str(votes).replace(',', '') + ',' + runtime + ',' + genre.replace(',', ' |').strip() + ',\n')
-
-f.close()
+    f.close()
